d823b350ef55d200b7f14fd7c5b9f1644ee73454
Jinki Jung <your.jinki.jung@gmail.com>
2022-01-17 16:03:33 +0100

    initial commit

---
+++ src/main/java/net/getko/iilregistry/IilRegistryApplication.java
@@ -0,0 +1,13 @@
+package net.getko.iilregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IilRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IilRegistryApplication.class, args);
+	}
+
+}
