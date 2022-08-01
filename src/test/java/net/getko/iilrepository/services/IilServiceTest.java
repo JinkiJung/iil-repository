081c3fd705cd910a1557800ef912aa79e2a81811
Jinki Jung <your.jinki.jung@gmail.com>
2022-08-01 11:43:35 +0200

    iilService testing

--- src/test/java/net/getko/iilrepository/services/IilServiceTest.java
+++ src/test/java/net/getko/iilrepository/services/IilServiceTest.java
@@ -2,11 +2,12 @@ package net.getko.iilrepository.services;
 
 import net.getko.iilrepository.models.domain.Iil;
 import net.getko.iilrepository.repositories.IilRepository;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,9 +35,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(MockitoExtension.class)
 public class IilServiceTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @InjectMocks
     @Spy
     private IilService iilService;
@@ -48,44 +47,47 @@ public class IilServiceTest {
     private Iil newIil;
     private Iil existingIil;
 
+    private UUID testUserID = UUID.randomUUID();
+
     @BeforeEach
     void setup() {
         this.iils = new ArrayList<>();
         for(long i=0; i<10 ; i++) {
             Iil iil = new Iil();
             iil.setId(UUID.randomUUID());
-            iil.setName("Test");
+            iil.setActor(testUserID.toString());
             iil.setAct("Go");
             this.iils.add(iil);
         }
 
         this.newIil = new Iil();
         this.newIil.setId(UUID.fromString("e91ab6d1-2585-422e-94a7-e538cbf284c3"));
-        this.newIil.setName("New iil");
+        this.newIil.setActor(testUserID.toString());
         this.newIil.setAct("Go");
 
         this.existingIil = new Iil();
         this.existingIil.setId(UUID.fromString("2110e38a-c9d4-43e1-ba5c-4741843281d1"));
-        this.existingIil.setName("Existing iil");
+        this.existingIil.setActor(testUserID.toString());
         this.existingIil.setAct("Stop");
     }
 
-    /*
-    @Test
-    void testFindAll() {
-
-        MvcResult mvcResult = this.mockMvc.perform(get("/api/iils"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(header().string("X-Total-Count", Long.toString(iils.length())))
-                .andExpect(header().exists(HttpHeaders.LINK))
-                .andReturn();
-    }
+    /**
+     * Test that we can retrieve all iils from the database
      */
 
     @Test
-    void testFindOne() {
-        doReturn(this.existingIil).when(iilRepository).findById(this.existingIil.getId());
+    public void testFindAll() {
+        List<Iil> iils = this.iils;
+        doReturn(iils).when(this.iilRepository).findAll();
+
+        List<Iil> result = this.iilService.findAll();
+
+        assertEquals(iils.size(), result.size());
+    }
+
+    @Test
+    public void testFindOne() {
+        doReturn(Optional.of(this.existingIil)).when(this.iilRepository).findById(this.existingIil.getId());
 
         // Perform the service call
         Iil result = this.iilService.findOne(this.existingIil.getId());
@@ -96,16 +98,15 @@ public class IilServiceTest {
         // Test the result
         assertNotNull(result);
         assertEquals(this.existingIil.getId(), result.getId());
-        assertEquals(this.existingIil.getName(), result.getName());
+        assertEquals(this.existingIil.getNamespace(), result.getNamespace());
         assertEquals(this.existingIil.getAct(), result.getAct());
         assertEquals(this.existingIil.getActor(), result.getActor());
-        assertEquals(this.existingIil.getEndWhen(), result.getEndWhen());
-        assertEquals(this.existingIil.getGiven(), result.getGiven());
-        assertEquals(this.existingIil.getStartWhen(), result.getStartWhen());
-        assertEquals(this.existingIil.getOwnedBy(), result.getOwnedBy());
-        assertEquals(this.existingIil.getProduce(), result.getProduce());
+        assertEquals(this.existingIil.getEndIf(), result.getEndIf());
+        assertEquals(this.existingIil.getInput(), result.getInput());
+        assertEquals(this.existingIil.getStartIf(), result.getStartIf());
+        assertEquals(this.existingIil.getOwner(), result.getOwner());
+        assertEquals(this.existingIil.getOutput(), result.getOutput());
         assertEquals(this.existingIil.getStatus(), result.getStatus());
-        assertEquals(this.existingIil.getCreatedBy(), result.getCreatedBy());
         assertEquals(this.existingIil.getCreatedAt(), result.getCreatedAt());
         assertEquals(this.existingIil.getLastUpdatedAt(), result.getLastUpdatedAt());
     }
